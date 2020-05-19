3e6c02c09003bef175158dacb5bebd7166afdcd0
xianggege <hx19581025@sina.com>
2020-05-19 22:05:59 +0800

    'Test1'

---
+++ src/main/java/com/kindo/mavendemo1/App.java
@@ -0,0 +1,13 @@
+package com.kindo.mavendemo1;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
